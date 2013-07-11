e4c4008ebd94862cd723876a80c9d30deb1836d8
tommasolevato <tommaso.levato@gmail.com>
2013-07-11 17:58:51 +0200

    Cleanup

--- DevisPutnamPython/src/main.py
+++ DevisPutnamPython/src/main.py
@@ -2,14 +2,14 @@ import randomGen
 import dpll
 
 def satProbability(n,m,k,num):
-    return randomGen.satProbability(n,m,k,num)
+    return randomGen.satProbability(n, m, k, num)
 
 def randomSentenceNum(n,m,k):
     symbols = randomGen.generateSymbols(n)
-    return randomGen.randomSentence(symbols,m,k)
+    return randomGen.randomSentence(symbols, m, k)
 
 def randomSentenceSym(symbols,m,k):
-    return randomGen.randomSentence(symbols,m,k)
+    return randomGen.randomSentence(symbols, m, k)
 
 def generateSymbols(n):
     return randomGen.generateSymbols(n)
